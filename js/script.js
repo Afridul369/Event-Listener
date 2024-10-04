075cc08b90d4d7f41a831aab87132ea7bd9428aa
Afridul369 <afridul.dev@gmail.com>
2024-10-04 20:44:17 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let cmc = document.querySelector('.abir');
+
+cmc.addEventListener('mouseover',()=>{
+
+   cmc.classList.add('css')
+
+})
+
+cmc.addEventListener('mouseout',()=>{
+
+   cmc.classList.remove('css')
+
+})
